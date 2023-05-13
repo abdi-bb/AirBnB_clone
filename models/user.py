6f3d55e0ead4b8f4207c878bc7466ad8f4526da0
Abdi Berhe <abdi201380@gmail.com>
2023-05-13 12:31:00 -0700

    class User

---
+++ models/user.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''
+module user
+'''
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    '''class User'''
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
+
+    def __init__(self, *args, **kwargs):
+        '''Constructor'''
+        super().__init__(*args, **kwargs)
