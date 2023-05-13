4a6f07d6b71efa63c48cdd72075448776dfebe1c
DannySanchez6658 <danieltsega6658@gmail.com>
2023-05-13 05:03:19 -0700

    [Feature]: Add a BaseModel class that is a parent of all othe class It defines 'id' and timestamps of when the object is created

---
+++ models/base_model.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+A Base Model Module
+"""
+
+
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """A Parent Base Model Class"""
+
+    def __init__(self):
+        """A Method that initializes
+        everytime object is created
+        """
+
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """A Method to return a dictionary
+        attribute
+        """
+
+        class_name = "[{}] ".format(self.__class__.__name__)
+        class_id = "({}) ".format(self.id)
+        class_dict = "{}".format(self.__dict__)
+        return class_name + class_id + class_dict
+
+    def save(self):
+        """A Method that updates the updated
+        at attribute
+        """
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """A Method that turns an object
+        to a dictionary
+        """
+
+        my_dict = self.__dict__
+        my_dict['__class__'] = self.__class__.__name__
+        my_dict['created_at'] = self.created_at.isoformat()
+        my_dict['updated_at'] = self.updated_at.isoformat()
+        return my_dict
