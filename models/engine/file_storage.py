31dece887ee9183c891475acd0b300e5946f7d1f
Abdi Berhe <abdi201380@gmail.com>
2023-05-14 03:19:58 -0700

    [Fix]: to catch TypeError and ValueError for reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -47,5 +47,5 @@ class FileStorage():
                     value['__class__'] = class_name
                     obj = eval(class_name)(**value)
                     FileStorage.__objects[key] = obj
-        except (ValueError, FileNotFoundError):
+        except (ValueError, FileNotFoundError, TypeError):
             pass
