d5ff37e495050ab7150238667bce915c79a23ab2
Abdi Berhe <abdi201380@gmail.com>
2023-05-15 10:52:46 -0700

    [Fix]: adding instantiation method

--- models/user.py
+++ models/user.py
@@ -12,3 +12,7 @@ class User(BaseModel):
     password = ''
     first_name = ''
     last_name = ''
+
+    def __init__(self, *args, **kwargs):
+        '''Constructor'''
+        super().__init__(*args, **kwargs)
